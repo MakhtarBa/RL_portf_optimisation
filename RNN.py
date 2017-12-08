a2d11b1f39c76d2ea30c0e0b6794d7ae1b7911d4
MakhtarBa <bamouhamadou001@gmail.com>
2017-12-07 21:36:11 -0500

    Add files via upload

--- RNN.py
+++ RNN.py
@@ -22,14 +22,14 @@ return_df=pd.read_csv('subset_return_df.csv')
 
 
 
-batch_size = 10
+number_batch = 500
 num_nodes = 2
 num_unrollings =  10
 rolling_window_size=100
 look_back=5# number of days to lookback, length of the input time series
 a=np.array([return_df['AAPL'][j:j+look_back].values for j in range(len(return_df['AAPL'])-look_back)],np.float32)
-delta=0.2 
-time=500
+delta=0.2
+time=100
     
 g = tf.Graph()
 
@@ -42,7 +42,7 @@ with g.as_default():
     input_data = []
     for i in range(num_unrollings):
         input_data.append(tf.placeholder(tf.float32,shape=(None,look_back),name='input'))
- 
+    
       #the actual signals 
     #output_data_feed= tf.placeholder(tf.float32,shape=(rolling_window_size-num_unrollings,None),name='one') #the minus one is to say that we input the real signal only on the previous days 
     
@@ -91,7 +91,7 @@ with g.as_default():
             a_ = tf.concat((tf.matmul(input_data[i],U),output_*W),axis=1)+b
             h_output = tf.nn.softmax(a_)
             output_after= tf.matmul(h_output,V)+c    
-                        
+            
         else:
             
             a_ = tf.concat((tf.matmul(input_data[i],U),output_after*W),axis=1)+b
@@ -110,7 +110,7 @@ with g.as_default():
     #log likelihood loss
     global_step = tf.Variable(0)
     L2_loss = tf.nn.l2_loss(U)
-    loss = tf.reduce_mean(observed_returns)+reg_tf*L2_loss
+    loss = -1*tf.reduce_mean(observed_returns)+reg_tf*L2_loss
     learning_rate = tf.train.exponential_decay(
         learning_rate=2.5,global_step=global_step, decay_steps=5000, decay_rate=0.1, staircase=True)
      
@@ -139,8 +139,8 @@ with g.as_default():
     sess.run(init)
     loss_value=[]
     
-    for j in range(batch_size):            
-        time=time+j
+    for j in range(number_batch):            
+        time=time+1
         feed_input={input_data[i]:a[time-num_unrollings+i].reshape(1,look_back) for i in range(num_unrollings)}
         grad_vals=sess.run(grad,feed_dict=feed_input)
         grad_, loss_tf_val=sess.run([grad_calcul, loss], feed_dict=feed_input)     
@@ -162,6 +162,6 @@ with g.as_default():
             update=sess.run(tf.norm(grad_val[0]*learning_rate))
             loss_value.append(loss_tf_val)
         '''    
-        
+              
 
 
